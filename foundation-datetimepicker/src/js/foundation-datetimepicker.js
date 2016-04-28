a8c65562cc7fb7b2850813144301a74d8d1b49f7
rubinoneroweb <rubinoneroweb@gmail.com>
2016-04-28 08:48:15 +0000

    Added it support

--- foundation-datetimepicker/src/js/foundation-datetimepicker.js
+++ foundation-datetimepicker/src/js/foundation-datetimepicker.js
@@ -1260,6 +1260,17 @@
 		suffix:      ["", "", "", ""],
 		today:       "Hoy"
 	},
+	
+		it: {
+		days:        ["Domenica", "Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato", "Domenica"],
+		daysShort:   ["Dom", "Lun", "Mar", "Mer", "Gio", "Ven", "Sab", "Dom"],
+		daysMin:     ["Do", "Lu", "Ma", "Me", "Gi", "Ve", "Sa", "Do"],
+		months:      ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"],
+		monthsShort: ["Gen", "Feb", "Mar", "Apr", "Mag", "Giu", "Lug", "Ago", "Set", "Ott", "Nov", "Dic"],
+		meridiem:    ["am", "pm"],
+		suffix:      ["", "", "", ""],
+		today:       "Oggi"
+	},
 
     zh: {
       days:        ["周日", "周一", "周二", "周三", "周四", "周五", "周六", "周日"],
